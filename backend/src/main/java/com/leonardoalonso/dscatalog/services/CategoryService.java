51fd83b79226fb5ccfbaa1f3c1e2e8477e810d4e
Leonardo-wl <leonardo.alonsot@hotmail.com>
2021-07-22 15:03:01 -0300

    DTO

--- backend/src/main/java/com/leonardoalonso/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/leonardoalonso/dscatalog/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.leonardoalonso.dscatalog.services;
 
+import com.leonardoalonso.dscatalog.dto.CategoryDTO;
 import com.leonardoalonso.dscatalog.entities.Category;
 import com.leonardoalonso.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -13,8 +16,9 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
-    public List<Category> findAll() {
-        return repository.findAll();
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
     }
-
 }
